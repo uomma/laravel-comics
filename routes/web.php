4578b23acb27aa37902de8b540c7155262d21be3
uomma <marcelloleone975@gmail.com>
2023-05-04 15:23:49 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/characters', function () {
+
+    $films =[
+        'movies' => config('db')
+    ];
+
+    return view('characters', $films);
+});
+
+Route::get('/comics', function () {
+    return view('home');
+});
+
+Route::get('/movies', function () {
+    return view('home');
+});
+
+Route::get('/tv', function () {
+    return view('home');
+});
+
+Route::get('/games', function () {
+    return view('home');
+});
+
+Route::get('/collectibles', function () {
+    return view('home');
+});
+
+Route::get('/videos', function () {
+    return view('home');
+});
+
+Route::get('/fans', function () {
+    return view('home');
+}); 
+
+Route::get('/news', function () {
+    return view('home');
+});
+
+Route::get('/shop', function () {
+    return view('home');
+});
